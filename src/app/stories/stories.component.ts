bdebfdbc9b5f118930ea9902ffe3f7d918727da2
lolptdr <jtingsan@gmail.com>
2016-09-22 07:06:35 -0500

    Add footer and stories components with styling

---
+++ src/app/stories/stories.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  templateUrl: './stories.component.html',
+  styleUrls: ['./stories.component.scss']
+})
+export class StoriesComponent implements OnInit {
+  items: number[];
+
+  constructor() {
+    this.items = Array(30).fill().map((x,i) => i);
+  }
+
+  ngOnInit() {
+  }
+
+}
