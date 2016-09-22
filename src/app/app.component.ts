a9afead532a9d4f913e5da20af582525a270bf6a
lolptdr <jtingsan@gmail.com>
2016-09-22 06:58:44 -0500

    Limit CSS styling affecting only component level with ViewEncapuslation.None

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = ' works!';
+
 }
