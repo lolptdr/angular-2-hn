dc453e4c69f3042f2f8adc579449e6e57fa2459d
lolptdr <jtingsan@gmail.com>
2016-09-22 07:21:12 -0500

    Create hn api service, link provider, use observable in stories component for new endpoint

---
+++ src/app/hackernews-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HackernewsApiService {
+  baseurl: string;
+
+  constructor(private http: Http) {
+    this.baseUrl = 'https://hacker-news.firebaseio.com/v0';
+  }
+
+  fetchStories(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/topstories.json`)
+                    .map(response => response.json());
+  }
+
+}
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { HackerNewsAPIService } from '../hackernews-api.service';
 
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.scss']
 })
-export class StoriesComponent implements OnInit {
-  items: number[];
 
-  constructor() {
-    this.items = Array(30).fill().map((x,i) => i);
-  }
+export class StoriesComponent implements OnInit {
+  items;
+
+  constructor(private _hackerNewsAPIService: HackerNewsAPIService) {}
 
   ngOnInit() {
+    this._hackerNewsApiService.fetchStories()
+      .subscirbe(
+        items => this.items = items,
+        error => console.log('Error fetching stories'));
   }
 
 }
